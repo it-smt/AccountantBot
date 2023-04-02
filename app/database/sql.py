ae81df31643d2ab106ffc8101a3d4b90209f024c
smt <smt_razrab@mail.ru>
2023-04-02 18:45:14 +0500

    sql

--- app/database/sql.py
+++ app/database/sql.py
@@ -10,22 +10,22 @@ class DB:
 
     def user_exists(self, user_id):
         """Проверяет, есть ли user в БД."""
-        query = self.cur.execute('SELECT id FROM users WHERE user_id = ?', (user_id,))
+        query = self.cur.execute('SELECT `id` FROM `users` WHERE `user_id` = ?', (user_id,))
         return bool(len(query.fetchall()))
 
     def get_user_id(self, user_id):
         """Получаем id пользователя в базе по его user_id."""
-        query = self.cur.execute('SELECT id FORM users WHERE user_id = ?', (user_id,))
-        return query.fetchall()[0]
+        query = self.cur.execute('SELECT `id` FROM users WHERE user_id = ?', (user_id,))
+        return query.fetchone()[0]
 
-    def add_user(self, user_id):
+    def add_user(self, user_id, username):
         """Добавляем пользователя в БД."""
-        self.cur.execute('INSERT INTO users (user_id) VALUES (?)', (user_id,))
+        self.cur.execute('INSERT INTO `users` (user_id, username) VALUES (?, ?)', (user_id, username))
         return self.conn.commit()
 
     def add_record(self, user_id, operation, value):
         """Добавляет новую запись о доходе/расходе в БД."""
-        self.cur.execute('INSERT INTO records (user_id, operation, value) VALUES (?, ?, ?)', (
+        self.cur.execute('INSERT INTO `records` (user_id, operation, value) VALUES (?, ?, ?)', (
             self.get_user_id(user_id),
             operation == '+',
             value
@@ -37,7 +37,7 @@ class DB:
         if within == 'day':
             # За последний день.
             result = self.cur.execute(
-                'SELECT * FROM records WHERE user_id = ? AND date BETWEEN datetime("now", "start of day") AND datetime("now", "localtime") ORDER BY date',
+                'SELECT * FROM `records` WHERE `user_id` = ? AND date BETWEEN datetime("now", "start of day") AND datetime("now", "localtime") ORDER BY date',
                 (self.get_user_id(user_id),)
             )
         elif within == 'month':
@@ -50,12 +50,6 @@ class DB:
             # За последний год.
             result = self.cur.execute(
                 'SELECT * FROM records WHERE user_id = ? AND date BETWEEN datetime("now", "start of month") AND datetime("now", "localtime") ORDER BY date',
-                (self.get_user_id(user_id),)
-            )
-        else:
-            # За все время.
-            result = self.cur.execute(
-                'SELECT * FROM records WHERE user_id = ? ORDER BY date',
                 (self.get_user_id(user_id),)
             )
 
